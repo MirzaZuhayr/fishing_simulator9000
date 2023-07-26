c75944d2c41500b367c4b73da3f0dad49a1d6335
Trey Bernard <bernard_tkb2@icloud.com>
2023-07-26 14:57:58 -0400

    movement update
    made movement 3d, also added drag and speed limit. taking a mini break/gonna work on animations/rotations later today/early tmr.

--- Fishing Simulator 9k/Assets/movement.cs
+++ Fishing Simulator 9k/Assets/movement.cs
@@ -4,12 +4,63 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    public float speed = 2f;
+    //movement
+    public Rigidbody rb;
+    public float moveSpeed;
+    public Transform orientation;
 
+    public float horInput;
+    public float vertInput;
+
+    Vector3 moveDirection;
+    //grounded?
+    public float playerHeight;
+    public LayerMask whereGround;
+    bool grounded;
+    public float groundDrag;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void readInput()
+    {
+        horInput = Input.GetAxisRaw("Horizontal");
+        vertInput = Input.GetAxisRaw("Vertical");
+    }
     void Update()
     {
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = transform.forward * moveVertical * speed;
-        transform.Translate(movement * Time.deltaTime);
+        //check if on ground
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whereGround);
+        readInput();
+        //drag control
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+        speedLimit();
+    }
+    private void Move()
+    {
+        moveDirection = orientation.forward * vertInput + orientation.right * horInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+    private void speedLimit()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
