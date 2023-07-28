6accf662914d25d4bf14970d315d3d9cd8af150c
MirzaZuhayr <rafijzuhayr@gmail.com>
2023-07-27 22:09:44 -0400

    wip: fishing button

---
+++ Fishing Simulator 9k/Assets/fishing_interaction.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+public class fishing_interaction : MonoBehaviour
+{   
+    public Text scoreTExt
+    wl;gadkrgn; hjbtedjlb hgnftrm.kkn
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void private void OnTriggerEnter(Collider other) {
+
+        
+    }
+}
